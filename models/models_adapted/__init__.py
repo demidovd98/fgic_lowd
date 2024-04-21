2d09dacb4bdcc93873a68f223067c7e9fbb98f2e
demidovd98 <demidovdv98@gmail.com>
2024-04-21 13:48:53 +0400

    locally adapt baselines output for our approach (return both embedding and class prediction)

---
+++ models/models_adapted/__init__.py
@@ -0,0 +1,15 @@
+from .resnet_my import *
+from .googlenet_my import *
+from .inception_my import *
+
+# from .alexnet import *
+# from .vgg import *
+# from .squeezenet import *
+# from .densenet import *
+# from .mobilenet import *
+# from .mnasnet import *
+# from .shufflenetv2 import *
+# from . import segmentation
+# from . import detection
+# from . import video
+# from . import quantization
